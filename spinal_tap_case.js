5b95d75b1dd8028142df1ec8fc2cf995f9cb6f4b
Anh Lai <anh.lai-ngoc@tek-experts.com>
2017-06-15 09:18:28 +0700

    add Advanced Code Solution

--- spinal_tap_case.js
+++ spinal_tap_case.js
@@ -1,5 +1,5 @@
 // Spinal Tap Case
-
+// This is my solution
 function spinalCase(str) {
   var regex = /[A-Z]/g;
   
@@ -9,6 +9,14 @@ function spinalCase(str) {
   
   if(newStr.charAt(0) === '-') newStr = newStr.substr(1);
   return newStr;
+}
+
+// This is Advanced Code Solution
+function betterSpinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+
+  return str.split(/\s|_|(?=[A-Z])/).join('-').toLowerCase()
 }
 
 spinalCase('Teletubbies say Eh-oh');
